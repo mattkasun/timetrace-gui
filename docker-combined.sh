b2192dc3276721d4d44fb0e0bd20ecf079e5bd13
Matthew R Kasun <mkasun@nusak.ca>
2021-11-24 14:54:49 -0500

    replace backtic with  $(STATEMENT)

--- docker-combined.sh
+++ docker-combined.sh
@@ -7,9 +7,9 @@ else
 fi
 echo $TAG 
 
-VERSION=`curl -fsSLI -o /dev/null -w %{url_effective} https://github.com/mattkasun/timetrace-gui/releases/latest | awk -F/ '{print $8}'`
+VERSION=$(curl -fsSLI -o /dev/null -w %{url_effective} https://github.com/mattkasun/timetrace-gui/releases/latest | awk -F/ '{print $8}')
 echo $VERSION
-VER=`curl -fsSLI -o /dev/null -w %{url_effective} https://github.com/dominikbraun/timetrace/releases/latest | awk -F/ '{print $8}'`
+VER=$(curl -fsSLI -o /dev/null -w %{url_effective} https://github.com/dominikbraun/timetrace/releases/latest | awk -F/ '{print $8}')
 echo $VER
 echo "docker build -t combined:v0.1 -f Dockerfile.combined . --build-arg VERSION=${VERSION} --build-arg VER=${VER}"
 docker build -t combined:${TAG} -f Dockerfile.combined . --build-arg VERSION=${VERSION} --build-arg VER=${VER}
