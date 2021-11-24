569aa62467abe9de32e1ddfe5deef41cee907e56
Matthew R Kasun <mkasun@nusak.ca>
2021-11-24 09:21:22 -0500

    tag argument for docker-combined.sh

--- docker-combined.sh
+++ docker-combined.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
+if [ -z "$1" ]
+then
+    TAG=$1
+else
+    TAG="testing"
+fi
+
 VERSION=`curl -fsSLI -o /dev/null -w %{url_effective} https://github.com/mattkasun/timetrace-gui/releases/latest | awk -F/ '{print $8}'`
 echo $VERSION
 VER=`curl -fsSLI -o /dev/null -w %{url_effective} https://github.com/dominikbraun/timetrace/releases/latest | awk -F/ '{print $8}'`
 echo $VER
 echo "docker build -t combined:v0.1 -f Dockerfile.combined . --build-arg VERSION=${VERSION} --build-arg VER=${VER}"
-docker build -t combined:v0.1 -f Dockerfile.combined . --build-arg VERSION=${VERSION} --build-arg VER=${VER}
+docker build -t combined:${TAG} -f Dockerfile.combined . --build-arg VERSION=${VERSION} --build-arg VER=${VER}
